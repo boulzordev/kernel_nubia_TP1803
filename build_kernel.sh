2a3bda986b63c19e312335cf897b2958826adade
Anay Wadhera <awadhera@berkeley.edu>
2020-09-02 21:25:00 -0700

    allow building to out

--- build_kernel.sh
+++ build_kernel.sh
@@ -18,8 +18,8 @@ if [[ "${1}" == "skip" ]] ; then
 	echo "Skipping Compilation"
 else
 	echo "Compiling kernel"
-	cp defconfig .config
-	make "$@" || exit 1
+	cp defconfig out/.config
+	make -j16 O=out/ "$@" || exit 1
 fi
 
 echo "Building new ramdisk"
@@ -47,7 +47,7 @@ cd $KERNELDIR
 
 echo "Making new boot image"
 mkbootimg \
-    --kernel arch/arm64/boot/Image.gz-dtb \
+    --kernel out/arch/arm64/boot/Image.gz-dtb \
     --cmdline 'console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 buildvariant=user printk.devkmsg=on' \
     --base           0x00000000 \
     --pagesize       4096 \
